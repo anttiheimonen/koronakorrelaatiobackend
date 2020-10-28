9a2bc34a5ad04b9e2f8a902a951cc516c2131033
Antti Heimonen <anttiheimonen@hotmail.com>
2020-10-28 13:14:15 +0200

    osoitteeseen /data voi lähettää GET-parametrit

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ console.log('hello world')
 const PORT = 8000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>palvelin on käynnissä</h1>')
+
+})
+
+app.get('/data/', (req, res) => {
+  // Otetaan osoitteen mukana tuleeet arvot muuttujiin
+  // esim. localhost:8000/data?kunta=turku&hakusana=kissa
+  const kunta = req.query.kunta
+  const hakusana = req.query.hakusana
+  res.send('<h1>Pyysit kuntaa: ' + kunta + ' ' + hakusana + '!</h1>')
+
 })
 
 
