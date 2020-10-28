b8ffdb016aa7898f0dd0970a2e625c00e5ed86c3
Antti Heimonen <anttiheimonen@hotmail.com>
2020-10-28 14:38:24 +0200

    Lisätty google haku /gtrends polkuun

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const googleTrends = require('google-trends-api');
+
 console.log('hello world')
 
 const PORT = 8000
@@ -9,13 +11,39 @@ app.get('/', (req, res) => {
 
 })
 
-app.get('/data/', (req, res) => {
+app.get('/gtrends/', (req, res) => {
   // Otetaan osoitteen mukana tuleeet arvot muuttujiin
-  // esim. localhost:8000/data?kunta=turku&hakusana=kissa
+  // esim. localhost:8000/gtrends?kunta=turku&hakusana=kissa
   const kunta = req.query.kunta
   const hakusana = req.query.hakusana
-  res.send('<h1>Pyysit kuntaa: ' + kunta + ' ' + hakusana + '!</h1>')
 
+  console.log(hakusana);
+  googleTrends.interestByRegion
+    ({
+      keyword: hakusana,
+      startTime: new Date('2020-08-08'),
+      endTime: new Date('2020-10-25'),
+      geo: 'FI',
+      resolution: 'city'
+    })
+    //keyword: document.getElementById("Hakukenttä").value})
+    .then(function (res) {
+      var receivedData = res.toString()
+      receivedData = JSON.parse(receivedData)
+      console.log(receivedData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.send('<h1>Pyysit kuntaa: ' + kunta + ' ' + hakusana + '!</h1>')
+})
+
+
+app.get('/google/', (req, res) => {
+  // Otetaan osoitteen mukana tuleeet arvot muuttujiin
+  // esim. localhost:8000/data?kunta=turku&hakusana=kissa
+  const hakusana = req.query.hakusana
+  res.send('<h1>Pyysit kuntaa: ' + hakusana + '!</h1>')
 })
 
 
