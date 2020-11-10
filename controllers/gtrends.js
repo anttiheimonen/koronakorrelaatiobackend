3097bdb57352479d2bf785c066e7139de5d65c93
vilma-p <72434725+vilma-p@users.noreply.github.com>
2020-11-11 00:22:50 +0200

    Näyttää jotenkin kunnan ja sen valuen

--- controllers/gtrends.js
+++ controllers/gtrends.js
@@ -1,9 +1,10 @@
 const { response } = require('express');
 const googleTrends = require('google-trends-api');
 const gtrendsRouter = require('express').Router();
+const kuntakoodit = require('./../utility/luettelo.json');
 
-
-gtrendsRouter.get('/interestOverTime', async (req, res) => {
+//TÄMÄ SE EDELLINEN TOIMIVA KOODI!
+/* gtrendsRouter.get('/', async (req, res) => {
   // Otetaan osoitteen mukana tuleeet arvot muuttujiin
   // esim. localhost:8000/gtrends?kunta=turku&hakusana=kissa
   const kunta = req.query.kunta
@@ -14,36 +15,8 @@ gtrendsRouter.get('/interestOverTime', async (req, res) => {
     ({
       keyword: hakusana,
       //trendDate: new Date(Date.now() - (14 * 24 * 60 * 60 * 1000)),
-      //startTime: new Date('2020-08-08'),
-      //endTime: new Date('2020-10-25'),
-      geo: 'FI',
-      resolution: 'city',
-    })
-    .then(function (googleRes) {
-      var receivedData = googleRes.toString()
-      receivedData = JSON.parse(receivedData)
-      console.log(receivedData)
-      res.json(receivedData)
-    })
-    .catch((err) => {
-      console.log(err)
-    });
-  // res.send('<h1>Pyysit kuntaa: ' + kunta + ' ' + hakusana + '!</h1>')
-})
-
-gtrendsRouter.get('/', async (req, res) => {
-  // Otetaan osoitteen mukana tuleeet arvot muuttujiin
-  // esim. localhost:8000/gtrends?kunta=turku&hakusana=kissa
-  const kunta = req.query.kunta
-  const hakusana = req.query.hakusana
-
-  console.log(hakusana);
-  googleTrends.interestOverTime
-    ({
-      keyword: hakusana,
-      startTime: new Date(Date.now() - (14 * 24 * 60 * 60 * 1000)),
-      //startTime: new Date('2020-08-08'),
-      //endTime: new Date('2020-10-25'),
+      startTime: new Date('2020-08-08'),
+      endTime: new Date('2020-10-25'),
       geo: 'FI',
       resolution: 'city'
     })
@@ -58,6 +31,72 @@ gtrendsRouter.get('/', async (req, res) => {
     });
   // res.send('<h1>Pyysit kuntaa: ' + kunta + ' ' + hakusana + '!</h1>')
 })
+*/
 
+//Tämä kokeilu ainakin jotenkin tulostaa vain sen kaupungin ja valuen arvon.
+//Ei vielä yhdisty kunnat ja koodit luettelo.json tiedostosta
+gtrendsRouter.get('/', async (req, res) => {
+  // Otetaan osoitteen mukana tuleeet arvot muuttujiin
+  // esim. localhost:8000/gtrends?kunta=turku&hakusana=kissa
+  const kunta = req.query.kunta
+  const hakusana = req.query.hakusana
+
+  console.log(hakusana);
+  googleTrends.interestByRegion
+    ({
+      keyword: hakusana,
+      //trendDate: new Date(Date.now() - (14 * 24 * 60 * 60 * 1000)),
+      startTime: new Date('2019-10-25'),
+      endTime: new Date(Date.now()),
+      geo: 'FI',
+      resolution: 'city'
+    })
+    .then(function (googleRes) {
+      var receivedData = googleRes.toString()
+      receivedData = JSON.parse(receivedData)
+        let valmisData = [];
+        var i;
+        for(i = 0; i < receivedData.default.geoMapData.length; i++) {
+          var geoName = receivedData.default.geoMapData[i].geoName;
+          var value = receivedData.default.geoMapData[i].value;
+          valmisData += [geoName + " : " + value + ", "].join(",");
+        }
+       console.log(valmisData)
+       res.json(valmisData)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  // res.send('<h1>Pyysit kuntaa: ' + kunta + ' ' + hakusana + '!</h1>')
+})
+
+
+/* gtrendsRouter.get('/', async (req, res) => {
+  // Otetaan osoitteen mukana tuleeet arvot muuttujiin
+  // esim. localhost:8000/gtrends?kunta=turku&hakusana=kissa
+  const kunta = req.query.kunta
+  const hakusana = req.query.hakusana
+
+  console.log(hakusana);
+  googleTrends.interestOverTime
+    ({
+      keyword: hakusana,
+      startTime: new Date(Date.now() - (14 * 24 * 60 * 60 * 1000)),
+      //startTime: new Date('2020-08-08'),
+      //endTime: new Date('2020-10-25'),
+      geo: 'FI'
+    })
+    .then(function (googleRes) {
+      var receivedData = googleRes.toString()
+      receivedData = JSON.parse(receivedData)
+      console.log(receivedData)
+      res.json(receivedData)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  // res.send('<h1>Pyysit kuntaa: ' + kunta + ' ' + hakusana + '!</h1>')
+})
+*/
 
 module.exports = gtrendsRouter
